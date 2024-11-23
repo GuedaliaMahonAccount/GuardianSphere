40bd3fccc8a179cba85655923258d819053e114b
Guedalia Sebbah <gsebbah@g.jct.ac.il>
2024-11-23 23:40:35 +0200

    fix backend

--- guardian_sphere_server/src/app.js
+++ guardian_sphere_server/src/app.js
@@ -4,12 +4,9 @@ const passport = require('passport');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require('path');
+require('dotenv').config(); // Charge les variables d'environnement depuis le fichier .env
 
-// Import routes
-
-const frontendRoutes = require('./routes/frontendRoutes'); // Import frontend routes
-
-const app = express();
+const app = express(); // Déclarez `app` au début, avant d'utiliser ses fonctionnalités
 
 // CORS configuration
 app.use(cors({
@@ -23,23 +20,50 @@ app.use(passport.initialize());
 app.use(bodyParser.json());
 
 // Session configuration
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }, // Set to true if using HTTPS
-  })
-);
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'default_secret_key', // Utilisez une clé de secours si SESSION_SECRET est manquant
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }, // Mettez à true si vous utilisez HTTPS
+}));
 
+
+
+
+
+
+// Import routes
+const frontendRoutes = require('./routes/frontendRoutes'); // Import frontend routes
+//
+//
+//
+//
+//
+//
 // Passport configuration
 require('./config/passport')(passport);
 
+
+
 // Backend routes
-
-
+// app.use('/api/users', userRoutes);
+//
+//
+//
+//
+//
+//
+//
+//
 // Frontend routes to serve files dynamically
-app.use('/', frontendRoutes); // Add the frontend routes for React files
+app.use('/', frontendRoutes); 
+
+
+
+
+
+
+
 
 // Health check route
 app.get('/health', (req, res) => {

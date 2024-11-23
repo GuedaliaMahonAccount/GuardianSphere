fc93b556425337abf8c400e0f1a0037ddfdbe46e
Guedalia Sebbah <gsebbah@g.jct.ac.il>
2024-11-23 23:30:27 +0200

    fix to 5001

--- guardian_sphere_server/server.js
+++ guardian_sphere_server/server.js
@@ -8,7 +8,7 @@ const connectDB = require('./src/config/db.js');
 // Connect to MongoDB
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
